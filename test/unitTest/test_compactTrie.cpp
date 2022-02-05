62c20128d5cf7324cea2f017ea6ba07290a65a62
Marta Pacuszka <pacuszka.m@gmail.com>
2022-02-05 23:53:29 +0100

    Handle duplicate keys in compact trie (#3)

--- test/unitTest/test_compactTrie.cpp
+++ test/unitTest/test_compactTrie.cpp
@@ -78,6 +78,42 @@ class CompactTrieUnitTest : public ::testing::Test {
         ASSERT_FALSE(trie.lookupPrefix("bannnns"));
     }
 
+    TEST_F(CompactTrieUnitTest, duplicateKey) {
+        std::vector<std::string> keys = {
+                "ba",
+                "baba",
+                "babca",
+                "banana",
+                "banana",
+                "bananas"
+        };
+        auto trie = CompactTrie(keys);
+
+        // true positives
+        ASSERT_TRUE(trie.lookupPrefix("b"));
+        ASSERT_TRUE(trie.lookupPrefix("ba"));
+        ASSERT_TRUE(trie.lookupPrefix("bab"));
+        ASSERT_TRUE(trie.lookupPrefix("baba"));
+        ASSERT_TRUE(trie.lookupPrefix("babc"));
+        ASSERT_TRUE(trie.lookupPrefix("babca"));
+        ASSERT_TRUE(trie.lookupPrefix("ban"));
+        ASSERT_TRUE(trie.lookupPrefix("bana"));
+        ASSERT_TRUE(trie.lookupPrefix("banan"));
+        ASSERT_TRUE(trie.lookupPrefix("banana"));
+        ASSERT_TRUE(trie.lookupPrefix("bananas"));
+
+        // false positives
+        ASSERT_TRUE(trie.lookupPrefix("bann"));
+        ASSERT_TRUE(trie.lookupPrefix("bannn"));
+
+        // negatives
+        ASSERT_FALSE(trie.lookupPrefix("c"));
+        ASSERT_FALSE(trie.lookupPrefix("ca"));
+        ASSERT_FALSE(trie.lookupPrefix("bb"));
+        ASSERT_FALSE(trie.lookupPrefix("bannnn"));
+        ASSERT_FALSE(trie.lookupPrefix("bannnns"));
+    }
+
 } // namespace compact_trie_test
 } // namespace range_filtering
 
